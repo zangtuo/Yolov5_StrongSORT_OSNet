e77faee43117ddacebbf2d85307bd6cc146307dd
lingfliu <lingfeng.liu@163.com>
2023-01-09 00:22:19 +0800

    updated

--- track_func.py
+++ track_func.py
@@ -148,7 +148,7 @@ def run(
                 pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det, nm=32)
             else:
                 pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
-            
+
 
         # Process detections
         for i, det in enumerate(pred):  # detections per image
@@ -177,7 +177,7 @@ def run(
             imc = im0.copy() if save_crop else im0  # for save_crop
 
             annotator = Annotator(im0, line_width=line_thickness, example=str(names))
-            
+
             if hasattr(tracker_list[i], 'tracker') and hasattr(tracker_list[i].tracker, 'camera_update'):
                 if prev_frames[i] is not None and curr_frames[i] is not None:  # camera motion compensation
                     tracker_list[i].tracker.camera_update(prev_frames[i], curr_frames[i])
@@ -202,11 +202,11 @@ def run(
                 # pass detections to strongsort
                 with dt[3]:
                     outputs[i] = tracker_list[i].update(det.cpu(), im0)
-                
+
                 # draw boxes for visualization
                 if len(outputs[i]) > 0:
                     for j, (output) in enumerate(outputs[i]):
-    
+
                         bbox = output[0:4]
                         id = output[4]
                         cls = output[5]
@@ -244,11 +244,11 @@ def run(
                             if save_crop:
                                 txt_file_name = txt_file_name if (isinstance(path, list) and len(path) > 1) else ''
                                 save_one_box(bbox.astype(np.int16), imc, file=save_dir / 'crops' / txt_file_name / names[c] / f'{id}' / f'{p.stem}.jpg', BGR=True)
-                            
+
             else:
                 pass
                 #tracker_list[i].tracker.pred_n_update_all_tracks()
-                
+
             # Stream results
             im0 = annotator.result()
             if show_vid:
@@ -277,7 +277,7 @@ def run(
                 vid_writer[i].write(im0)
 
             prev_frames[i] = curr_frames[i]
-            
+
         # Print total time (preprocessing + inference + NMS + tracking)
         LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{sum([dt.dt for dt in dt if hasattr(dt, 'dt')]) * 1E3:.1f}ms")
 
@@ -291,84 +291,213 @@ def run(
         strip_optimizer(yolo_weights)  # update model (to fix SourceChangeWarning)
 
 
-def parse_opt():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--yolo-weights', nargs='+', type=Path, default=WEIGHTS / 'yolov5s-seg.pt', help='model.pt path(s)')
-    parser.add_argument('--reid-weights', type=Path, default=WEIGHTS / 'osnet_x0_25_msmt17.pt')
-    parser.add_argument('--tracking-method', type=str, default='bytetrack', help='strongsort, ocsort, bytetrack')
-    parser.add_argument('--tracking-config', type=Path, default=None)
-    parser.add_argument('--source', type=str, default='0', help='file/dir/URL/glob, 0 for webcam')  
-    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
-    parser.add_argument('--conf-thres', type=float, default=0.5, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.5, help='NMS IoU threshold')
-    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--show-vid', action='store_true', help='display tracking video results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
-    parser.add_argument('--save-trajectories', action='store_true', help='save trajectories for each track')
-    parser.add_argument('--save-vid', action='store_true', help='save video tracking results')
-    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
-    # class 0 is person, 1 is bycicle, 2 is car... 79 is oven
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--visualize', action='store_true', help='visualize features')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default=ROOT / 'runs/track', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--line-thickness', default=2, type=int, help='bounding box thickness (pixels)')
-    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
-    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
-    parser.add_argument('--hide-class', default=False, action='store_true', help='hide IDs')
-    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
-    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
-    parser.add_argument('--vid-stride', type=int, default=1, help='video frame-rate stride')
-    parser.add_argument('--retina-masks', action='store_true', help='whether to plot masks in native resolution')
-    opt = parser.parse_args()
-    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
-    opt.tracking_config = ROOT / 'trackers' / opt.tracking_method / 'configs' / (opt.tracking_method + '.yaml')
-    print_args(vars(opt))
-    return opt
+@torch.no_grad()
+def track(
+        source='0',
+        yolo_weights=WEIGHTS / 'yolov5n.pt',  # model.pt path(s),
+        reid_weights=WEIGHTS / 'osnet_x0_25_msmt17.pt',  # model.pt path,
+        tracking_method='bytetrack',
+        tracking_config=None,
+        imgsz=(640, 640),  # inference size (height, width)
+        conf_thres=0.25,  # confidence threshold
+        iou_thres=0.45,  # NMS IOU threshold
+        max_det=1000,  # maximum detections per image
+        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        save_txt=False,  # save results to *.txt
+        save_conf=False,  # save confidences in --save-txt labels
+        save_crop=False,  # save cropped prediction boxes
+        save_trajectories=False,  # save trajectories for each track
+        save_vid=False,  # save confidences in --save-txt labels
+        nosave=False,  # do not save images/videos
+        classes=None,  # filter by class: --class 0, or --class 0 2 3
+        agnostic_nms=False,  # class-agnostic NMS
+        augment=False,  # augmented inference
+        visualize=False,  # visualize features
+        update=False,  # update all models
+        project=ROOT / 'runs/track',  # save results to project/name
+        name='exp',  # save results to project/name
+        exist_ok=False,  # existing project/name ok, do not increment
+        line_thickness=2,  # bounding box thickness (pixels)
+        hide_labels=False,  # hide labels
+        hide_conf=False,  # hide confidences
+        hide_class=False,  # hide IDs
+        half=False,  # use FP16 half-precision inference
+        dnn=False,  # use OpenCV DNN for ONNX inference
+        vid_stride=1,  # video frame-rate stride
+        retina_masks=False,
+):
+    source = str(source)
+    is_file = Path(source).suffix[1:] in (VID_FORMATS)
+    is_url = source.lower().startswith(('rtsp://', 'rtmp://', 'http://', 'https://'))
+    webcam = source.isnumeric() or source.endswith('.txt') or (is_url and not is_file)
+    if is_url and is_file:
+        source = check_file(source)  # download, TODO: save into specific directories
+
+    #TODO: define save_dir for result saving
+    file_name = source.lower().split('\\')[-1]
+    if is_file:
+        save_dir = os.path.join(ROOT, file_name)
+    else:
+        save_dir = None
+
+    save_dir = Path(save_dir)
+    # Load model
+    device = select_device(device)
+    is_seg = '-seg' in str(yolo_weights)
+    model = DetectMultiBackend(yolo_weights, device=device, dnn=dnn, data=None, fp16=half)
+    stride, names, pt = model.stride, model.names, model.pt
+    imgsz = check_img_size(imgsz, s=stride)  # check image size
+
+    # Dataloader
+    if webcam:
+        show_vid = check_imshow()
+        dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
+        nr_sources = len(dataset)
+    else:
+        dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt)
+        nr_sources = 1
+    vid_path, vid_writer, txt_path = [None] * nr_sources, [None] * nr_sources, [None] * nr_sources
+
+    # Create as many strong sort instances as there are video sources
+    tracker_list = []
+    for i in range(nr_sources):
+        tracker = create_tracker(tracking_method, tracking_config, reid_weights, device, half)
+        tracker_list.append(tracker, )
+        if hasattr(tracker_list[i], 'model'):
+            if hasattr(tracker_list[i].model, 'warmup'):
+                tracker_list[i].model.warmup()
+    outputs = [None] * nr_sources
+
+    # Run tracking
+    # model.warmup(imgsz=(1 if pt else nr_sources, 3, *imgsz))  # warmup
+    seen, windows, dt = 0, [], (Profile(), Profile(), Profile(), Profile())
+    curr_frames, prev_frames = [None] * nr_sources, [None] * nr_sources
+
+    track_result = []
+    for frame_idx, (path, im, im0s, vid_cap, s) in enumerate(dataset):
+        with dt[0]:
+            im = torch.from_numpy(im).to(device)
+            im = im.half() if half else im.float()  # uint8 to fp16/32
+            im /= 255.0  # 0 - 255 to 0.0 - 1.0
+            if len(im.shape) == 3:
+                im = im[None]  # expand for batch dim
+
+        # Inference
+        with dt[1]:
+            # visualize = increment_path(save_dir / Path(path[0]).stem, mkdir=True) if visualize else False
+            if is_seg:
+                pred, proto = model(im, augment=augment, visualize=visualize)[:2]
+            else:
+                pred = model(im, augment=augment, visualize=False) # not visualize as service
+
+        # Apply NMS
+        with dt[2]:
+            if is_seg:
+                pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det, nm=32)
+            else:
+                pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            seen += 1
+            if webcam:  # nr_sources >= 1
+                p, im0, _ = path[i], im0s[i].copy(), dataset.count
+                p = Path(p)  # to Path
+                s += f'{i}: '
+                save_path = str(save_dir / p.name)  # im.jpg, vid.mp4, ...
+            else:
+                p, im0, _ = path, im0s.copy(), getattr(dataset, 'frame', 0)
+                p = Path(p)  # to Path
+                # video file
+                if source.endswith(VID_FORMATS):
+                    save_path = str(save_dir / p.name)  # im.jpg, vid.mp4, ...
+                # folder with imgs
+                else:
+                    save_path = str(save_dir / p.parent.name)  # im.jpg, vid.mp4, ...
+            curr_frames[i] = im0
+
+            if hasattr(tracker_list[i], 'tracker') and hasattr(tracker_list[i].tracker, 'camera_update'):
+                if prev_frames[i] is not None and curr_frames[i] is not None:  # camera motion compensation
+                    tracker_list[i].tracker.camera_update(prev_frames[i], curr_frames[i])
+
+            if det is not None and len(det):
+                if is_seg:
+                    # scale bbox first the crop masks
+                    if retina_masks:
+                        det[:, :4] = scale_boxes(im.shape[2:], det[:, :4],
+                                                 im0.shape).round()  # rescale boxes to im0 size
+                        masks = process_mask_native(proto[i], det[:, 6:], det[:, :4], im0.shape[:2])  # HWC
+                    else:
+                        masks = process_mask(proto[i], det[:, 6:], det[:, :4], im.shape[2:], upsample=True)  # HWC
+                        det[:, :4] = scale_boxes(im.shape[2:], det[:, :4],
+                                                 im0.shape).round()  # rescale boxes to im0 size
+                else:
+                    det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()  # rescale boxes to im0 size
+
+                # pass detections to strongsort
+                with dt[3]:
+                    outputs[i] = tracker_list[i].update(det.cpu(), im0)
+            else:
+                pass
+                # tracker_list[i].tracker.pred_n_update_all_tracks()
+
+            # Save results (image with detections)
+            if save_vid:
+                if vid_path[i] != save_path:  # new video
+                    vid_path[i] = save_path
+                    if isinstance(vid_writer[i], cv2.VideoWriter):
+                        vid_writer[i].release()  # release previous video writer
+                    if vid_cap:  # video
+                        fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                        w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                        h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                    else:  # stream
+                        fps, w, h = 30, im0.shape[1], im0.shape[0]
+                    save_path = str(Path(save_path).with_suffix('.mp4'))  # force *.mp4 suffix on results videos
+                    vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                vid_writer[i].write(im0)
+
+            prev_frames[i] = curr_frames[i]
+
+        track_result.append(outputs)
+
+    return track_result
+
+
+'''图像人群分析'''
+def track_img(source='img.jpeg', h=1024, w=960, save_result=False):
+    pass
 
 
 '''监控设备'''
-def cam(source='rtsp://11.11.11.11', h=1024, w=960):
+def track_webcam(url='rtsp://11.11.11.11', h=1024, w=960):
+    # track_method = 'bytetrack'
+    # track_config = ROOT / 'trackers' / track_method / 'configs' / (track_method + '.yaml')
+    # track(yolo_weights=Path('yolov5n.pt'), source='media\\vid3.mp4', save_trajectories=True, tracking_config=track_config)
     pass
 
+'''无人机视频车流分析'''
+def track_vid_drone_vehicle(source='media\\vid3.mp4', h=1024, w=960):
+    track_method = 'bytetrack'
+    track_config = ROOT / 'trackers' / track_method / 'configs' / (track_method + '.yaml')
+    result = track(yolo_weights=Path('yolov5n.pt'), source=source, save_trajectories=True,
+          tracking_config=track_config)
 
-'''图像分析'''
-def img(source='img.jpeg', h=1024, w=960):
-    pass
+    # TODO: 处理track结果
+    # 1. 绘制轨迹（基于类型+id：人或车辆）
+    # 2. 计算每个车辆的帧移动速度 v = trajactory / frames
+    # 3. 计算网格区域车辆 num / grid
+    # 4. 计算区域密度变化趋势:
+    #   a. 依据网格平均移动速度*时间 移动该网格内所有车辆至其他网格
+    #   b. 重新计算网格车辆密度：如出现车辆聚集，则生成异常判决
 
-'''视频分析'''
-def vid(source='vid.mp4', h=1024, w=960):
-    trace = [];
-    '''read video'''
 
-    '''detect & track'''
-    mot = track()
-    trace.append(mot);
+def gpr2d(obs1, obs2, target_pos):
+    obs1 = [[0,0, 45],[0,1,34]]
+    obs2 = [[0,0, 45],[0,1,34]]
+    target_pos [ [0,0], [0,1]]
 
-    '''calc vector speed'''
-
-    '''calc density'''
-
-    '''calc crowding 拥堵'''
-    for m in range(grid_w):
-        for n in range(grid_h):
-            '''find all velo in this area & neighbors'''
-            velo = [0, 1, 2]
-            v_avg = [0,0]
-            for v in velo:
-                v_avg += v[1:]
-            v_avg = np.average(np.array(v_avg)[1:,:]).toarray()
-
-    '''flow err 异常流动'''
-
-    return (density, flow, crowd, err)
+    return np.array([[0,0, 45],[0,1,34]])
 
 def density(img_url, grid_w, grid_h):
     return [
@@ -385,6 +514,5 @@ def density(img_url, grid_w, grid_h):
 #     run(**vars(opt))
 #
 #
-# if __name__ == "__main__":
-#     opt = parse_opt()
-#     main(opt)
+if __name__ == "__main__":
+    track_webcam()
