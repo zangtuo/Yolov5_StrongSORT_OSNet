56449ed702ab4e1e45b71b2c7758628a6e4bc59a
zangtuo <84187199+zangtuo@users.noreply.github.com>
2023-04-02 10:24:04 +0800

    update interface_people_img

--- Human.py
+++ Human.py
@@ -124,6 +124,7 @@ coco_classes = {
     90: "toothbrush"
 }
 
+
 class Hir_Yolo:
     """
     HirYOLO：YOLOV5检测人和物，ocsort跟踪， coco训练
@@ -354,6 +355,94 @@ class Hir_Yolo:
             #     return track_result
             track_result.extend(outputs)
         return track_result
+
+    @torch.no_grad()
+    def interface_people_img(self, im, im0s, path, s):
+        """
+        单帧的图片显示，返回数据，读取数据和处理数据的解耦合
+        :return:
+        """
+        hide_labels = False
+        hide_class = False
+        hide_conf = False
+        names = self.model.names
+        tracker = create_tracker(track_method, self.track_config, self.reid_weights, self.device, False)
+
+        track_result = []
+        seen, windows, dt = 0, [], (Profile(), Profile(), Profile(), Profile())
+        curr_frames, prev_frames = [None], [None]
+        outputs = [None]
+
+        w, h = [0, 0]
+        n_frame = 0
+
+        with dt[0]:
+            im = torch.from_numpy(im).to(self.device)
+            im = im.half() if False else im.float()  # uint8 to fp16/32
+            im /= 255.0  # 0 - 255 to 0.0 - 1.0
+            if len(im.shape) == 3:
+                im = im[None]  # expand for batch dim
+        # Inference
+        with dt[1]:
+            if self.slice_enabled:
+                # TODO: SAHI
+                pred = []
+            else:
+                pred = self.model(im, augment=False, visualize=False)
+
+        # Apply NMS
+        with dt[2]:
+            pred = non_max_suppression(pred, conf_thres=0.25, iou_thres=0.45, classes=None, agnostic=False,
+                                       max_det=1000)
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            p, im0 = path, im0s.copy()
+            p = Path(p)  # to Path
+            annotator = Annotator(im0, line_width=2, example=str(names))
+            curr_frames[i] = im0
+
+            if hasattr(tracker, 'tracker') and hasattr(tracker, 'camera_update'):
+                if prev_frames[i] is not None and curr_frames[i] is not None:  # camera motion compensation
+                    tracker.camera_update(prev_frames[i], curr_frames[i])
+
+            if det is not None and len(det):
+                det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()  # rescale boxes to im0 size
+
+                # pass detections to strongsort
+                with dt[3]:
+                    outputs[i] = tracker.update(det.cpu(), im)
+                if len(outputs[i]) > 0:
+                    for j, (output) in enumerate(outputs[i]):
+                        bbox = output[0:4]
+                        id = output[4]
+                        cls = output[5]
+                        conf = output[6]
+                        # data[::]->openpose
+                        # Add bbox to image
+                        c = int(cls)  # integer class
+                        id = int(id)  # integer id
+                        label = None if hide_labels else (f'{id} {names[c]}' if hide_conf else (
+                                                                  f'{id} {conf:.2f}' if hide_class else f'{id} {names[c]} {conf:.2f}'))
+                        color = colors(c, True)
+                        annotator.box_label(bbox, label, color=color)
+            # 画图
+            im0 = annotator.result()
+            if platform.system() == 'Linux' and p not in windows:
+                windows.append(p)
+                cv2.namedWindow(str(p), cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
+                cv2.resizeWindow(str(p), im0.shape[1], im0.shape[0])
+            cv2.imshow(str(p), im0)
+            if cv2.waitKey(1) == ord('q'):  # 1 millisecond
+                exit()
+            LOGGER.info(
+                f"{s}{'' if len(det) else '(no detections), '}{sum([dt.dt for dt in dt if hasattr(dt, 'dt')]) * 1E3:.1f}ms")
+        return outputs
+
+    def load_video(self, media_source):
+        stride, pt = self.model.stride, self.model.pt
+        dataset = LoadImages(media_source, img_size=self.imgsz, stride=stride)
+        return dataset
 
 
 # def interface_
